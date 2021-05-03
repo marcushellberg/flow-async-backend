035b782c3761c8126b5894618d07174ea24768c5
Marcus Hellberg <marcus@vaadin.com>
2021-05-03 14:35:36 -0700

    finished

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -2,12 +2,14 @@ package com.example.application.views.helloworld;
 
 import com.example.application.backend.BackendService;
 import com.example.application.views.main.MainView;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -18,19 +20,38 @@ public class HelloWorldView extends HorizontalLayout {
 
   private TextField nameField = new TextField("Your name");
   private Button button = new Button("Say hello", e -> save());
+  private ProgressBar progressBar = new ProgressBar();
 
   public HelloWorldView(BackendService service) {
     this.service = service;
     addClassName("hello-world-view");
 
-    add(nameField, button);
+    progressBar.setIndeterminate(true);
+    progressBar.setVisible(false);
+    progressBar.setWidth("200px");
+
+    add(nameField, button, progressBar);
     setAlignItems(Alignment.BASELINE);
   }
 
   private void save() {
-    service.save(nameField.getValue());
-    showNotification();
-    nameField.clear();
+    progressBar.setVisible(true);
+    button.setEnabled(false);
+    var ui = UI.getCurrent();
+
+    service.saveAsync(nameField.getValue()).addCallback(result -> {
+
+      ui.access(() -> {
+        progressBar.setVisible(false);
+        button.setEnabled(true);
+
+        showNotification();
+        nameField.clear();
+      });
+
+    }, err -> {
+      ui.access(() -> Notification.show("BOO"));
+    });
   }
 
   private void showNotification() {
